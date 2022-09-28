38f40a39f25f3345f8fa7783df431f3e1b7db29e
ellisvelandia <eyis619@gmail.com>
2022-09-27 19:49:29 -0500

    candy

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
+import { updateBoard } from "./store";
+import { useAppSelector, useAppDispatch } from "./store/hooks";
+import { createBoard } from "./utils/createBoard";
 
 function App() {
-  return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+  const dispatch = useAppDispatch();
+
+  const board = useAppSelector(({ candyCrush: { board } }) => board);
+  const boardSize = useAppSelector(
+    ({ candyCrush: { boardSize } }) => boardSize
   );
+
+  useEffect(() => {
+    // dispatch(updateBoard(createBoard(boardSize)));
+    console.log(createBoard(boardSize));
+  }, []);
+
+  return <div className="flex items-center justify-center h-screen"></div>;
 }
 
 export default App;
